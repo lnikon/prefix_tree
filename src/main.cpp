2a4c24bcfafe291ccabffc732d05916b1e1b05d7
nreplay <nreplay@list.ru>
2018-04-21 14:43:03 +0400

    added insert, find

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <string>
+#include "prefix_tree.h"
 
-int main() 
+int main()
 {
-  std::cout << "Hello there!\n";
+  Trie trie;
+
+  // Insert
+  auto node = trie.insert(std::string("async"), 4);
+  std::cout << "value: " << node.first->value;
+  for(const auto& ch : node.first->m_children)
+  {
+    std::cout << "child: " << ch.first;
+  }
+  std::cout << "\n";
+
+  // Find
+  auto foundAS = trie.find("as");
+  if(foundAS.second)
+  {
+    std::cout << "Found ""as""\n";
+  }
+  else
+  {
+    std::cout << "Doesn't found ""as""\n";
+  }
+
+  auto foundDEB = trie.find("deb");
+  if(foundDEB.second)
+  {
+    std::cout << "Found ""deb""\n";
+  }
+  else
+  {
+    std::cout << "Doesn't found ""deb""\n";
+  }
   return 0;
 }
