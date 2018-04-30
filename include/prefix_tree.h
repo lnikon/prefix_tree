89e4478e9d29cc60de1135f35ac50482557ec926
lnikon <vagbej@gmail.com>
2018-04-30 13:33:36 +0400

    made trie value generic

--- include/prefix_tree.h
+++ include/prefix_tree.h
@@ -9,17 +9,18 @@
 #include <memory>
 #include <iostream>
 
+template <class T>
 class Trie
 {
 public:
   struct Node
   {
     std::unordered_map<char, std::unique_ptr<Node>> m_children;
-    int value {0};
+    T value {0};
     Node(): m_children() {}
   };
 
-  Trie::Node* insert(std::string key, int value);
+  Trie::Node* insert(std::string key, T value);
   std::pair<Trie::Node*, bool> find(std::string key);
   void traverse();
 
@@ -34,5 +35,80 @@ private:
 
   void traverse(Node *node);
   void visit(Node *node);
-
 };
+
+template <class T>
+typename Trie<T>::Node* Trie<T>::insert(std::string key, T value)
+{
+    size_t i = 0;
+    Node *pNode = m_root.get();
+
+    while(i < key.length())
+    {
+        if(pNode->m_children.count(key[i]) == 0)
+        {
+            m_nodeCounter++;
+            pNode->m_children[key[i]] = std::make_unique<Trie::Node>();
+        }
+        pNode = pNode->m_children[key[i]].get();
+        i++;
+    }
+    pNode->value = value;
+
+    return pNode;
+}
+
+template <class T>
+std::pair<typename Trie<T>::Node*, bool> Trie<T>::find(std::string key)
+{
+    size_t i = 0;
+    Node *pNode = m_root.get();
+    bool exist = true;
+
+    while(i < key.length())
+    {
+        if(pNode->m_children.count(key[i]) != 0)
+        {
+            pNode = pNode->m_children.at(key[i]).get();
+            i++;
+        }
+        else
+        {
+            pNode = nullptr;
+            exist = false;
+            break;
+        }
+    }
+
+    return std::pair<Trie::Node *, bool>(pNode, exist);
+}
+
+template <class T>
+void Trie<T>::traverse()
+{
+    traverse(m_root.get());
+}
+
+template <class T>
+void Trie<T>::traverse(typename Trie<T>::Node *node)
+{
+    if(node == nullptr)
+    {
+        return;
+    }
+
+    for(size_t i = 0; i < node->m_children.size(); i++)
+    {
+        traverse(node->m_children[i].get());
+        visit(node->m_children[i].get());
+    }
+}
+
+template <class T>
+void Trie<T>::visit(typename Trie<T>::Node *node)
+{
+    if(node && node->value)
+    {
+        std::cout << node->value << '\n';
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
 #include <string>
 #include "prefix_tree.h"
-#include "trie_test.h"
 
 int main()
 {
-  Trie trie;
+  Trie<int> trie;
 
   trie.insert("anna", 19);
   trie.insert("anahit", 20);
+  trie.insert("nikol", 102);
 
   std::cout << "Nodes count: "
             << trie.getNodeCounter() << '\n';
   trie.traverse();
-
-  //  TRIE_TEST trie_test;
-
-  //  size_t count = 1000;
-  //  auto nodes = trie_test.TRIE_INSERT(trie, count);
-
-  //  for(const auto &node : nodes)
-  //  {
-  //    std::cout << node->value << ' ';
-  //  }
-  //  std::cout << '\n';
 
   return 0;
 }
